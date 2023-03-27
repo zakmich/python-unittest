9380e9737ab07f672866facc9b6f094f29b8c152
zakmich <WYZv7XbX@Jwuktb>
2023-03-27 21:44:20 +0200

    Class that provides simple calculations

---
+++ 08_parametrized_test/calculator/calc_simple.py
@@ -0,0 +1,19 @@
+class CalcSimple:
+    """Class that provides simple calculations"""
+
+    def add(self, x, y):
+        return x + y
+
+    def sub(self, x, y):
+        return x - y
+
+    def mul(self, x, y):
+        return x * y
+
+    def div(self, x, y):
+        return x / y
+
+
+# calculation = CalcSimple()
+#
+# print(calculation.div(12, 2))
