4d75f391578d71a485879cecdf6d572ab6964b12
zakmich <WYZv7XbX@Jwuktb>
2023-04-02 18:32:31 +0200

    Product class initial release

--- 09_shopping_basket_mini_project/shopping_basket/product.py
+++ 09_shopping_basket_mini_project/shopping_basket/product.py
@@ -1,1 +1,11 @@
+class Product:
+
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def __repr__(self):
+        return f"product name {self.name} product price {self.price} product quantity {self.quantity}"
+
 
